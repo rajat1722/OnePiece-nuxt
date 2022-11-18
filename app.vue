1841e137e25af9bb6127c0505d86893df6ae160a
Rajat Rana <rajat.rana@vidyamantra.com>
2022-11-18 11:10:07 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-3xl">One Piece</h1>
+    <p>
+      Follows the adventures of Monkey D. Luffy and his pirate crew in order to find the greatest treasure ever left by the legendary Pirate, Gold Roger. The famous mystery treasure named "One Piece".
+    </p>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/tailwindcss', [
+
+        '@pinia/nuxt',
+
+        {
+
+          autoImports: [
+
+            // automatically imports defineStore
+
+            'defineStore', // import { defineStore } from 'pinia'
+
+            // automatically imports defineStore as definePiniaStore
+
+            ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+
+          ],
+
+        },
+
+      ],
+
+    ],
+    css: [
+      "/layouts/global.css",
+    ]
+    
+
+})

3c0630799f0bb65b1e5983da148d13543d84a6f9
Rajat Rana <rajat.rana@vidyamantra.com>
2022-11-21 15:02:10 +0530

    created default error

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <img src="../assets/0b3509104454037.5f633f4ccbda7.jpg" alt="straw hat pirates" class="max-w-full absolute opacity-10 top-0 bottom-0 right-0 left-0">
         <h1 class="text-[4rem] text-center py-8">One Piece</h1>
         <p class=" w-2/4 mx-auto">
-        Follows the adventures of Monkey D. Luffy and his pirate crew in order to find the greatest treasure ever left by the legendary Pirate, Gold Roger. The famous mystery treasure named "One Piece".
+        Follows the adventures of <span class=" text-blue-700">Monkey D. Luffy</span> and his pirate crew in order to find the greatest treasure ever left by the legendary Pirate, Gold Roger. The famous mystery treasure named "One Piece".
         </p>
 
        
